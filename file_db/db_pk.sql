50d9782d24a5c9b32a79fcf5365e33ed355f5fe7
Rifqi Al Wafa <51861464+rifqialwafa@users.noreply.github.com>
2020-12-15 15:18:31 +0700

    new db

--- file_db/db_pk.sql
+++ file_db/db_pk.sql
@@ -1,27 +1,135 @@
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: db_pk
--- ------------------------------------------------------
--- Server version	5.5.5-10.4.13-MariaDB
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Des 2020 pada 09.16
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET NAMES utf8mb4 */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Database: `db_pk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_kendaraan`
+--
+
+CREATE TABLE `tb_kendaraan` (
+  `id_kendaraan` int(11) NOT NULL,
+  `nama_kendaraan` varchar(100) NOT NULL,
+  `plat` varchar(255) NOT NULL,
+  `warna` varchar(100) NOT NULL,
+  `merk` varchar(100) NOT NULL,
+  `jenis` varchar(100) NOT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_kendaraan`
+--
+
+INSERT INTO `tb_kendaraan` (`id_kendaraan`, `nama_kendaraan`, `plat`, `warna`, `merk`, `jenis`, `image`, `status`) VALUES
+(1, 'Ayla', 'F 1990 FF', 'Putih', 'Daihatsu', 'Mobil', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_peminjaman`
+--
+
+CREATE TABLE `tb_peminjaman` (
+  `id_peminjaman` int(11) NOT NULL,
+  `id_peminjam` varchar(100) NOT NULL,
+  `id_kendaraan` int(11) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `ket` varchar(255) NOT NULL,
+  `tanggal_peminjaman` timestamp NOT NULL DEFAULT current_timestamp(),
+  `tanggal_pengembalian` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_users`
+--
+
+CREATE TABLE `tb_users` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `id_pegawai` varchar(100) NOT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_users`
+--
+
+INSERT INTO `tb_users` (`id`, `nama`, `email`, `password`, `id_pegawai`, `role`) VALUES
+(1, 'Atasan', 'atasan@atas.com', 'e10adc3949ba59abbe56e057f20f883e', '112233qwerty', 1),
+(2, 'Karyawan', 'karyawan@karya.com', 'e10adc3949ba59abbe56e057f20f883e', '123123qwerty', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_kendaraan`
+--
+ALTER TABLE `tb_kendaraan`
+  ADD PRIMARY KEY (`id_kendaraan`);
+
+--
+-- Indeks untuk tabel `tb_peminjaman`
+--
+ALTER TABLE `tb_peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`);
+
+--
+-- Indeks untuk tabel `tb_users`
+--
+ALTER TABLE `tb_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_pegawai` (`id_pegawai`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_kendaraan`
+--
+ALTER TABLE `tb_kendaraan`
+  MODIFY `id_kendaraan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_peminjaman`
+--
+ALTER TABLE `tb_peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_users`
+--
+ALTER TABLE `tb_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-12-15 14:46:24
