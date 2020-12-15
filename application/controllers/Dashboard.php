1eae1a5d69aca4612d9a55ce4747994451b9f779
Diva Wardana <divawardana26@gmail.com>
2020-12-15 15:54:34 +0700

    Add template

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function index()
+	{
+		$data['title'] = 'Dashboard';
+
+        $this->load->view('layout/header', $data);
+        $this->load->view('layout/sidebar', $data);
+        $this->load->view('layout/topbar', $data);
+        $this->load->view('dashboard', $data);
+        $this->load->view('layout/footer');
+	}
+}
